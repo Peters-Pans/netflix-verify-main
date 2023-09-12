c5ebad24d20d3a7a2080b0b68bd06cade60771b6
Peters-Pans <69051746+Peters-Pans@users.noreply.github.com>
2023-09-12 18:49:20 +0800

    Update printer.go

--- printer/printer.go
+++ printer/printer.go
@@ -29,22 +29,22 @@ func printResult(ipVersion string, vResponse verify.VerifyResponse) {
 	fmt.Printf("[IPv%s]\n", ipVersion)
 	switch code := vResponse.StatusCode; {
 	case code < -1:
-		fmt.Println(RED_PREFIX + "您的网络可能没有正常配置IPv" + ipVersion + "，或者没有IPv" + ipVersion + "网络接入" )
+		fmt.Println("您的网络没有正常配置IPv" + ipVersion)
 	case code == -1:
-		fmt.Println(RED_PREFIX + "Netflix在您的出口IP所在的国家不提供服务" )
+		fmt.Println("Netflix在您的IP所在的国家不提供服务" )
 	case code == 0:
-		fmt.Println(RED_PREFIX + "Netflix在您的出口IP所在的国家提供服务，但是您的IP疑似代理，无法正常使用服务" )
-		fmt.Println(CYAN_PREFIX + "NF所识别的IP地域信息：" + vResponse.CountryName )
+		fmt.Println("Netflix在您的出口IP所在的国家提供服务，但是您的IP疑似代理，无法正常使用服务" )
+		fmt.Println("NF所识别的IP地域信息：" + vResponse.CountryName )
 	case code == 1:
-		fmt.Println(YELLOW_PREFIX + "您的出口IP可以使用Netflix，但仅可看Netflix自制剧" )
-		fmt.Println(CYAN_PREFIX + "NF所识别的IP地域信息：" + vResponse.CountryName )
+		fmt.Println("您的出口IP可以使用Netflix，但仅可看Netflix自制剧" )
+		fmt.Println("NF所识别的IP地域信息：" + vResponse.CountryName )
 	case code == 2:
-		fmt.Println(GREEN_PREFIX + "您的出口IP完整解锁Netflix，支持非自制剧的观看" )
-		fmt.Println(CYAN_PREFIX + "NF所识别的IP地域信息：" + vResponse.CountryName )
+		fmt.Println("您的出口IP完整解锁Netflix，支持非自制剧的观看" )
+		fmt.Println("NF所识别的IP地域信息：" + vResponse.CountryName )
 	case code == 3:
-		fmt.Println(YELLOW_PREFIX + "您的出口IP无法观看此电影" )
+		fmt.Println("您的出口IP无法观看此电影" )
 	case code == 4:
-		fmt.Println(GREEN_PREFIX + "您的出口IP可以观看此电影" )
+		fmt.Println("您的出口IP可以观看此电影" )
 		fmt.Println(CYAN_PREFIX + "NF所识别的IP地域信息：" + vResponse.CountryName )
 	default:
 		fmt.Println(YELLOW_PREFIX + "解锁检测失败，请稍后重试" )
